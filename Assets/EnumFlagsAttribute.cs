a6beb5e037b46432d58505472af370d108656d7a
Ejaz Ahmed <ejazahmed0195@gmail.com>
2020-02-01 15:41:14 +0530

    updated player controller

---
+++ Assets/EnumFlagsAttribute.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class EnumFlagsAttribute: PropertyAttribute {
+	public string enumName;
+	
+	public EnumFlagsAttribute() {}
+
+	public EnumFlagsAttribute(string name) {
+		enumName = name;
+	}
+}
